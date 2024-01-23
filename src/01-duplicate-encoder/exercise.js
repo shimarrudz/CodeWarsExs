b2580432e24eed083684f6d8289c762f1b5aa52b
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-23 15:35:53 -0300

    feat: start exercise

---
+++ src/01-duplicate-encoder/exercise.js
@@ -0,0 +1,11 @@
+function duplicateEncode(word) {
+    word = word.toLowerCase();
+
+    let result = '';
+    const charCount = {};
+
+    for (let i = 0; i < word.lenght; i++) {
+        const char = word[i];
+        charCount[char] = 
+    }
+}
