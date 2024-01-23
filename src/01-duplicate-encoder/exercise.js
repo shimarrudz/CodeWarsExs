fc01d43d7f480cee6323bd96bbf76974410bf3dc
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-23 20:44:57 -0300

    feat: finish exercise

--- src/01-duplicate-encoder/exercise.js
+++ src/01-duplicate-encoder/exercise.js
@@ -1,11 +1,28 @@
 function duplicateEncode(word) {
     word = word.toLowerCase();
-
+    
     let result = '';
     const charCount = {};
-
-    for (let i = 0; i < word.lenght; i++) {
-        const char = word[i];
-        charCount[char] = 
+    
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+      charCount[char] = (charCount[char] || 0) + 1;
     }
-}
+    
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+      if (charCount[char] === 1) {
+        result += '(';
+      } else {
+        result += ')';
+      }
+    }
+    
+    return result;
+  }
+  
+
+console.log(duplicateEncode("din"));       // Deve retornar "((("
+console.log(duplicateEncode("recede"));    // Deve retornar "()()()"
+console.log(duplicateEncode("Success"));   // Deve retornar ")())())"
+console.log(duplicateEncode("(( @"));      // Deve retornar "))(("
