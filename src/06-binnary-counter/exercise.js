6985279406a224c6e848b0eea56860483ea5df1b
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-25 16:04:01 -0300

    feat: add exercise

--- src/06-binnary-counter/exercise.js
+++ src/06-binnary-counter/exercise.js
@@ -1,6 +1,8 @@
 function countBits(number) {
     // Convert the interger to its binary representation and count '1's
     // toString(2) coverts a number to a binnary string
+    // split('0') to divide the string in an array that separate '0' and '1's
+    // join('') to unify the substrings in a only string, removing all '0's then we got '11111'
     return number.toString(2).split('0').join('').length;
 }
 
